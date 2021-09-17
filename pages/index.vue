f3a3c6840b73aa76dec17fec16520f1f5704bde7
saboyutaka <yutaka.paradigm.shift@gmail.com>
2021-09-17 12:09:14 +0000

    wp-sample/posts にredirect する

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,15 @@
 <template>
   <div>
-    <h1>Posts</h1>
-    <ul>
-      <li v-for="title in titles" :key="title">
-        {{ title }}
-      </li>
-    </ul>
+    <h1>CodeforOkinawa</h1>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import { WP_REST_API_Posts } from "wp-types";
-
-interface Data {
-  posts: WP_REST_API_Posts;
-}
 
 export default Vue.extend({
-  data(): Data {
-    return {
-      posts: [],
-    };
-  },
-
-  async asyncData({ $wpapi }): Promise<Data> {
-    const posts: WP_REST_API_Posts = await $wpapi.posts().get();
-    return { posts };
-  },
-
-  computed: {
-    titles(): string[] {
-      return this.posts.map(p => p.title.rendered);
-    },
+  middleware({ redirect }) {
+    redirect("/wp-sample/posts");
   },
 });
 </script>
